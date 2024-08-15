d38576c1c322974c8977e5d7cf616e01a64e1c4b
ManoelFer <manoelfernandes15@gmail.com>
2024-08-15 13:34:58 -0300

    feat: readme files

--- src/app/notes/[[...id]]/_components/UpdateNote/index.tsx
+++ src/app/notes/[[...id]]/_components/UpdateNote/index.tsx
@@ -11,7 +11,7 @@ export async function UpdateNote({ id }: { id: string }) {
   return (
     <main className="m-10 space-y-12">
       <div className="my-5">
-        <h1 className="m-5 text-center">Edit note</h1>
+        <h1 className="m-5 text-center text-3xl text-gray-50">Edit note</h1>
         <NotesForm action={updateAction} data={note} />
       </div>
 
